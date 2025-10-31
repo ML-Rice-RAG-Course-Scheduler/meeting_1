4033d7e528613eb5ad6b286031d2e7a8ef57c1b2
wjin1 <74127131+wjin1@users.noreply.github.com>
2025-10-31 16:23:16 -0500

    Update gitpractive.py

--- gitpractive.py
+++ gitpractive.py
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 
+
+
+=======
+# Simply add another print statement below with your name!
 print("David Nyari")
+print("Ryan Pascual")
+print("Caleb Song")
+print("Grace Yang")
 print("Wendy Jin")
 
+>>>>>>> 27529d1bf6241d62583f5c12d6f899d5f1e707eb
